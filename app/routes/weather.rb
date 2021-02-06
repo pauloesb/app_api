5a5569aff695835813834664d652c150bd413fc1
Paulo Borba <pauloesb@gmail.com>
2021-02-06 17:29:20 -0300

    Add the two required endpoints with validations

--- app/routes/weather.rb
+++ app/routes/weather.rb
@@ -1,12 +1,26 @@
 module Weather
   class API < Grape::API
     format :json
-    prefix :weather
 
-    get :summary do
-    end
+    namespace :weather do
+      params do
+        requires :unit, type: Symbol, values: [:celsius, :fahrenheit]
+        requires :locations, type: String
+      end
 
-    get :locations do
+      get :summary do
+        { unit: params[:unit], locations: params[:locations] }
+      end
+
+      namespace :locations do
+        params do
+          requires :location_id, type: Integer
+        end
+
+        get '/:location_id' do
+          { test: params[:location_id] }
+        end
+      end
     end
   end
 end
