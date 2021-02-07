5f649dc9df81ab7c5a784249028d260c5a78acde
Paulo Borba <pauloesb@gmail.com>
2021-02-07 10:44:14 -0300

    Define default response if route not found

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
 class App < Grape::API
   mount Weather::API
+  format :json
+
+  route :any, '*path' do
+    error!({ error: 'Not Found', detail: "No such route '#{request.path}'" }, 404)
+  end
 end
