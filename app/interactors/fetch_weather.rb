f113dbf2293d5cf9450bb21922599ee2d7defa2d
Paulo Borba <pauloesb@gmail.com>
2021-02-07 13:00:29 -0300

    Refactor to improve fetch_weather logic

--- app/interactors/fetch_weather.rb
+++ app/interactors/fetch_weather.rb
@@ -1,14 +1,16 @@
 class FetchWeather
   include Interactor
 
-  WEATHER_PATH = '/weahter'
+  WEATHER_PATH = '/weather'
+
+  before do
+    context.unit ||= :fahrenheit
+  end
   
   def call
-    if data = requester.get(WEATHER_PATH, query_data(context))
-      context.data = data
-    else
-      context.fail!
-    end
+    fetch_weather_location
+
+    context.fail! unless is_success?
   end
 
   private
@@ -17,7 +19,39 @@ class FetchWeather
     @requester ||= OpenWeatherMapRequester.new
   end
 
-  def query_data(context)
-    { id: context.location_id }
+  def query_data
+    { id: context.location_id }.merge(temperature_units.fetch(context.unit))
+  end
+
+  def is_success?
+    context.status == 200
+  end
+
+  def temperature_units
+    {
+      celsius: { units: 'metric' },
+      fahrenheit: { units: 'imperial' }
+    }
+  end
+
+  def fetch_weather_location
+    response = requester.get(WEATHER_PATH, query_data)
+
+    context.status = fetch_status(response)
+    context.result = fetch_response_data(response[:data])
+  end
+
+  def fetch_status(response)
+    response.fetch(:status, 500)
+  end
+
+  def fetch_response_data(data)
+    {
+      city: data.fetch(:name),
+      temperature: data[:main].fetch(:temp),
+      unit: context.unit
+    }
+  rescue KeyError
+    data
   end
 end
